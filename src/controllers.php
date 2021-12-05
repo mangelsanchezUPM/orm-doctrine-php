09ae3776c6c2b4a8ea36885209d3ecd9dfcdead5
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-12-05 18:20:17 +0100

    First commit

---
+++ src/controllers.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * ResultsDoctrine - controllers.php
+ *
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es/ ETS de Ingeniería de Sistemas Informáticos
+ */
+
+use MiW\Results\Entity\User;
+use MiW\Results\Utility\DoctrineConnector;
+
+function funcionHomePage()
+{
+    global $routes;
+
+    $rutaListado = $routes->get('ruta_user_list')->getPath();
+    echo <<< ____MARCA_FIN
+    <ul>
+        <li><a href="$rutaListado">Listado Usuarios</a></li>
+    </ul>
+____MARCA_FIN;
+}
+
+function funcionListadoUsuarios(): void
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+
+    $userRepository = $entityManager->getRepository(User::class);
+    $users = $userRepository->findAll();
+    var_dump($users);
+}
+
+function funcionUsuario(string $name)
+{
+    echo $name;
+}
