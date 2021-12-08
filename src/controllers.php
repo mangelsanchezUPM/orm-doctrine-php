98fb82bfaa7ccd3153ec3c84beaaa6d4a4a7d8f1
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-12-08 14:12:25 +0100

    Crear usuario

--- src/controllers.php
+++ src/controllers.php
@@ -16,13 +16,13 @@ function funcionHomePage()
     global $routes;
 
     $rutaListadoUsuario = $routes->get('ruta_user_list')->getPath();
-    $rutaUsuario = $routes->get('ruta_user')->getPath();
+    $rutaFormCrearUsuario = $routes->get('ruta_create_user_form')->getPath();
     $rutaListadoResultados = $routes->get('ruta_result_list')->getPath();
 
     echo <<< ____MARCA_FIN
     <ul>
         <li><a href="$rutaListadoUsuario">Listado Usuarios</a></li>
-        <li><a href="$rutaUsuario">Listado Resultados</a></li>
+        <li><a href="$rutaFormCrearUsuario">Crear Usuario</a></li>
         <li><a href="$rutaListadoResultados">Listado Resultados</a></li>
     </ul>
 ____MARCA_FIN;
@@ -44,6 +44,63 @@ function funcionUsuario(string $name)
     $userRepository = $entityManager->getRepository(User::class);
     $user = $userRepository->findOneBy(['username' => $name]);
     var_dump($user);
+}
+
+function formCrearUsuario()
+{
+    global $routes;
+    $rutaFormCrearUsuario = $routes->get('ruta_create_user')->getPath();
+
+    echo <<< ____MARCA_FIN
+    
+    <form method="post" action="$rutaFormCrearUsuario">
+        <table>
+            <tr><td><label>Nombre de usuario: </label></td> 
+                <td><input type="text" name="username" required></td></tr>
+            <tr><td><label>Email: </label></td> 
+                <td><input type="email" name="email" required></td></tr>
+            <tr><td><label>Contraseña: </label></td>
+                <td><input type="password" name="password" required></td></tr>
+            <tr><td><label>Habilitado: </label></td> 
+                <td><input type="checkbox" name="enabled" checked></td></tr>
+            <tr><td><label>Es Admin: </label></td> 
+                <td><input type="checkbox" name="isAdmin"></td></tr>
+            <tr><td colspan="2"><button type="submit">Crear</button></td></tr>
+        </table>
+    </form>
+____MARCA_FIN;
+}
+
+function funcionCrearUsuario()
+{
+    $newUser = new User();
+    if (isset($_POST)) {
+        if (isset($_POST['username'])) {
+            $newUser->setUsername($_POST['username']);
+        }
+        if (isset($_POST['email'])) {
+            $newUser->setEmail($_POST['email']);
+        }
+        if (isset($_POST['password'])) {
+            $newUser->setPassword($_POST['password']);
+        }
+        if (isset($_POST['enabled'])) {
+            $newUser->setEnabled(true);
+        }
+        if (isset($_POST['isAdmin'])) {
+            $newUser->setIsAdmin(true);
+        }
+        $entityManager = DoctrineConnector::getEntityManager();
+        try {
+            $entityManager->persist($newUser);
+            $entityManager->flush();
+            var_dump($newUser);
+        } catch (Throwable $exception) {
+            echo $exception->getMessage() . PHP_EOL;
+        }
+    } else {
+        echo "Usuario no generado";
+    }
 }
 
 function funcionBorrarUsuario(string $name)
