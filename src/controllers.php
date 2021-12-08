670a657115d7ae7441644fc02bcb51b0cbf79be9
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-12-08 11:58:47 +0100

    Listado de usuarios y resultados

--- src/controllers.php
+++ src/controllers.php
@@ -8,16 +8,22 @@
  */
 
 use MiW\Results\Entity\User;
+use MiW\Results\Entity\Result;
 use MiW\Results\Utility\DoctrineConnector;
 
 function funcionHomePage()
 {
     global $routes;
 
-    $rutaListado = $routes->get('ruta_user_list')->getPath();
+    $rutaListadoUsuario = $routes->get('ruta_user_list')->getPath();
+    $rutaUsuario = $routes->get('ruta_user')->getPath();
+    $rutaListadoResultados = $routes->get('ruta_result_list')->getPath();
+
     echo <<< ____MARCA_FIN
     <ul>
-        <li><a href="$rutaListado">Listado Usuarios</a></li>
+        <li><a href="$rutaListadoUsuario">Listado Usuarios</a></li>
+        <li><a href="$rutaUsuario">Listado Resultados</a></li>
+        <li><a href="$rutaListadoResultados">Listado Resultados</a></li>
     </ul>
 ____MARCA_FIN;
 }
@@ -33,5 +39,57 @@ function funcionListadoUsuarios(): void
 
 function funcionUsuario(string $name)
 {
+    $entityManager = DoctrineConnector::getEntityManager();
     echo $name;
+    $userRepository = $entityManager->getRepository(User::class);
+    $user = $userRepository->findOneBy(['username' => $name]);
+    var_dump($user);
+}
+
+function funcionBorrarUsuario(string $name)
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+    $userRepository = $entityManager->getRepository(User::class);
+    $user = $userRepository->findBy(['username' => $name])[0];
+    try {
+        $entityManager->remove($user);
+        $entityManager->flush();
+        echo "Usuario borrado: " . PHP_EOL;
+        var_dump($user);
+    } catch (Throwable $exception) {
+        echo $exception->getMessage() . PHP_EOL;
+    }
+}
+
+function funcionResultados()
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+
+    $resultRepository = $entityManager->getRepository(Result::class);
+    $results = $resultRepository->findAll();
+    var_dump($results);
+}
+
+function funcionResultado(string $result)
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+    echo "$result";
+    $resultRepository = $entityManager->getRepository(Result::class);
+    $results = $resultRepository->findBy(['result' => $result]);
+    var_dump($results);
+}
+
+function funcionBorrarResultado(string $name)
+{
+    $entityManager = DoctrineConnector::getEntityManager();
+    $resultRepository = $entityManager->getRepository(Result::class);
+    $result = $resultRepository->findOneBy(['result' => $name]);
+    try {
+        $entityManager->remove($result);
+        $entityManager->flush();
+        echo "Resultado borrado: " . PHP_EOL;
+        var_dump($result);
+    } catch (Throwable $exception) {
+        echo $exception->getMessage() . PHP_EOL;
+    }
 }
